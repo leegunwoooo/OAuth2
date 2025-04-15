863dc224777027f50b011865c8840815c23e95e2
leegunwoooo <leegunwoo0325@gmail.com>
2025-04-15 15:27:23 +0900

    Feat :: 환경설정

---
+++ src/test/java/poppop/oauth/OAuthApplicationTests.java
@@ -0,0 +1,13 @@
+package poppop.oauth;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OAuthApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
